4a9283e1cda7b904e1dc9b29a85a5c2b428761e3
jay4497 <jay4497@126.com>
2024-01-04 17:13:41 +0800

    init

---
+++ src/components/Book.vue
@@ -0,0 +1,15 @@
+<template>
+    <a-card hoverable v-model="book">
+        <template #cover="scope">
+            <img :alt="scope.title" :src="scope.cover">
+        </template>
+        <a-card-meta :title="book.title" :description="book.description"></a-card-meta>
+    </a-card>
+</template>
+
+<script setup>
+    import { ref, defineProps } from 'vue'
+
+    const props = defineProps(['data'])
+    const book = props.data
+</script>
