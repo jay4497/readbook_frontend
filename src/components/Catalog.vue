6734f3f5e638e8f681d4f6fee0666e6749fac39b
jay4497 <jay4497@126.com>
2024-01-10 11:56:21 +0800

    dev: catalog & content

---
+++ src/components/Catalog.vue
@@ -0,0 +1,32 @@
+<template>
+    <a-list bordered :data-source="rows">
+        <template #renderItem="{ item }">
+            <a-list-item @click="checkContent(item)">
+                <template #actions>
+                    <RightOutlined />
+                </template>
+                <a-list-item-meta>
+                    <template #title>
+                        {{ item.title }}
+                    </template>
+                </a-list-item-meta>
+            </a-list-item>
+        </template>
+    </a-list>
+</template>
+
+<script setup>
+    import { useRouter } from 'vue-router'
+
+    const { push } = useRouter()
+    const props = defineProps(['rows'])
+    const rows = props.rows
+
+    const checkContent = (item) => {
+        if (item.type === 'content') {
+            push({ name: 'Content' })
+        } else {
+            push({ name: 'Catalog' })
+        }
+    }
+</script>
---
+++ src/components/Content.vue
@@ -0,0 +1,39 @@
+<template>
+    <div ref="container" class="content"></div>
+    <Player :source="audioSrc" />
+</template>
+
+<script setup>
+
+    const props = defineProps(['content'])
+    const audioSrc = ref()
+    const container = ref()
+    const contentList = []
+    let paragraph = document.createElement('p')
+    if (props.contnet) {
+        props.contnet.forEach((item) => {
+            const span = document.createElement('span')
+            span.setAttribute('data-audio', item.audio)
+            span.innerText = item.text
+            span.addEventListener('click', (e) => {
+                const all_spans = document.querySelectorAll('.content span')
+                all_spans.forEach((el) => {
+                    el.style.fontWeight = 'normal'
+                })
+                e.target.style.fontWeight = 'bold'
+                audioSrc.value = e.target.getAttribute('data-audio')
+            })
+            paragraph.append(span)
+            if (item.wrap === 1) {
+                contentList.push(paragraph)
+                paragraph = document.createElement('p')
+            }
+        })
+
+        if (contentList.length > 0) {
+            contentList.forEach((item) => {
+                container.value.append(item)
+            })
+        }
+    }
+</script>
